af968cb947ac1a6196b088c62ca23f5f4e34a195
lxhyl <3529725395@qq.com>
2019-12-19 15:36:24 +0800

    sf

--- 算法/2019-12-19.js
+++ 算法/2019-12-19.js
@@ -20,9 +20,9 @@
             var j = array.length - 1;
             while (i < j) {
                 sum = item + array[i] + array[j];
-                 
-               //因为数组已经排序好了
-               //如果新值大于旧值，把j向前移位     
+
+                //因为数组已经排序好了
+                //如果新值大于旧值，把j向前移位     
                 if (Math.abs(sum - target) > cha) {
                     j--;
                 } else {
@@ -33,7 +33,7 @@
                     result = sum;
                     i++;
                 }
-               
+
 
             }
 
@@ -43,4 +43,64 @@
     }
     threeSum([-1, 2, 1, -4], 1)
 
+}
+
+{
+    //给定一个包含 n 个整数的数组 nums 和一个目标值 target，
+    //判断 nums 中是否存在四个元素 a，b，c 和 d ，
+    //使得 a + b + c + d 的值与 target 相等？
+    //找出所有满足条件且不重复的四元组。
+
+    // 注意：
+
+    // 答案中不可以包含重复的四元组。
+
+    // 示例：
+
+    // 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+
+    // 满足要求的四元组集合为：
+    // [
+    //   [-1,  0, 0, 1],
+    //   [-2, -1, 1, 2],
+    //   [-2,  0, 0, 2]
+    // ]
+
+    //4数之和
+    var fourSum = function (arr, target) {
+        arr.sort((a, b) => {
+            return a - b;
+        })
+        var result = [];
+        for (let i = 0; i < (arr.length - 3); i++) {
+            for (let j = i + 1; j < (arr.length - 2); j++) {
+                var start = j + 1;
+                var end = arr.length - 1;
+                while (start < end) {
+
+                    var sum = arr[i] + arr[j] + arr[start] + arr[end];
+                  
+                    if (sum == target) {
+                        let ans = [arr[i], arr[j], arr[start], arr[end]];
+                        result.push(ans);
+                        end--;
+                       
+                    }
+                    if (sum > target) {
+                        end--;
+                      
+                    }
+                    if(sum<target){
+                        start++;
+                    }
+
+                }
+            }
+        }
+       
+        console.log(result);
+
+    }
+
+    fourSum([1, 0, -1, 0, -2, 2], 0)
 }
