053841af1c06ba9807f3bb340bd225a2a7acb9c5
lxhyl <zhangpengfan6@gmail.com>
2019-12-24 14:33:56 +0800

    sf

--- 算法/2019-12-23.js
+++ 算法/2019-12-23.js
@@ -11,21 +11,54 @@
     // 输入: ["dog","racecar","car"]
     // 输出: ""
     // 解释: 输入不存在公共前缀。
-    var longestCommonPrefix = function(strs) {
-        if(strs.length == 0) 
+
+    //1.
+    var longestCommonPrefix = function (strs) {
+        if (strs.length == 0)
             return "";
         let ans = strs[0];
-        for(let i =1;i<strs.length;i++) {
-            let j=0;
-            for(;j<ans.length && j < strs[i].length;j++) {
-                if(ans[j] != strs[i][j])
+        for (let i = 1; i < strs.length; i++) {
+
+            for (var j = 0; j < ans.length && j < strs[i].length; j++) {
+                if (ans[j] != strs[i][j])
                     break;
             }
             ans = ans.substr(0, j);
-            if(ans === "")
+            if (ans === "")
                 return ans;
         }
         return ans;
     };
-    console.log(longestCommonPrefix(["flower","flow11","flowet"]));
+    console.log(longestCommonPrefix(["flower", "flow11", "flowet"]));
+
+    // 2.
+    var longestCommonPrefix = function (strs) {
+        if (strs.length == 0) {
+            return ''
+        }
+        var num = 1;
+        var common = strs[0].substr(0, num);
+        var i = 0;
+        var result = '';
+        while (i < strs.length) {
+            if (strs[i].startsWith(common)) {
+                i++;
+                if (i == strs.length - 1) {
+                    result = common;
+                    i = 0;
+                    if (num < strs[i].length) {
+                        common = strs[0].substr(0, num++);
+                       
+                    } else {
+                        break
+                    }
+                }
+            } else {
+                break
+            }
+       }  
+       return result;
+    }
+    console.log(longestCommonPrefix(["flower", "flow11", "flowet"]));
+
 }
