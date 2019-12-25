c070a666cdc309948d4bd7714940a444f65ea449
lxhyl <zhangpengfan6@gmail.com>
2019-12-25 16:44:00 +0800

    tx

--- 算法/2019-12-25.js
+++ 算法/2019-12-25.js
@@ -27,38 +27,126 @@
 // 输出: 4193
 // 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。
 
-var atoi = function(str){
-   var strTrim = str.trim();
-   var strArr = strTrim.split('');
-   strArr = strArr.filter(item => {
-       return item.trim();
-   })
-   var fh = '';
-   var result = '';
-   var i =0;
-   if(strArr[0] == '-'){
-       strArr.shift();
-       fh = '-';
-   }else if(isNaN(strArr[0])){
-       return 0;
-   }
-   while(i<strArr.length){
-       if(!isNaN(strArr[i])){  
-         result =  result.concat(strArr[i])
-         i++;
-       }else {
-          break;
-       }
-   }
-   if(result > Math.pow(2,31) - 1){
-       if(fh == '-'){
-        return "INT_MIN(2^-31)";
-       }
-       if(fh == ''){
-       return "INT_MAX(2^31-1)";
-       }
-   }
-   return result = fh + result;
- 
+var atoi = function (str) {
+    var strTrim = str.trim();
+    var strArr = strTrim.split('');
+    strArr = strArr.filter(item => {
+        return item.trim();
+    })
+    var fh = '';
+    var result = '';
+    var i = 0;
+    if (strArr[0] == '-') {
+        strArr.shift();
+        fh = '-';
+    } else if (isNaN(strArr[0])) {
+        return 0;
+    }
+    while (i < strArr.length) {
+        if (!isNaN(strArr[i])) {
+            result = result.concat(strArr[i])
+            i++;
+        } else {
+            break;
+        }
+    }
+    if (result > Math.pow(2, 31) - 1) {
+        if (fh == '-') {
+            return "INT_MIN(2^-31)";
+        }
+        if (fh == '') {
+            return "INT_MAX(2^31-1)";
+        }
+    }
+    return result = fh + result;
+
 }
 console.log(atoi("  - 4 42 1111with words"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 给定一个包含非负整数的 m x n 网格，
+// 请找出一条从左上角到右下角的路径，
+// 使得路径上的数字总和为最小。
+
+// 说明：每次只能向下或者向右移动一步。
+
+// 示例:
+
+// 输入:
+// [
+//   [1,3,1],
+//   [1,5,1],
+//   [4,2,1]
+// ]
+// 输出: 7
+// 解释: 因为路径 1→3→1→1→1 的总和最小。
+
+//贪心算法
+var minPathSum = function (arr) {
+    var sum = 0;
+    var result = [];
+    var pathSum = arr[0].length + arr.length - 1;
+    var step = 0;
+    var i = 0, j = 0;
+    var right = 0, bottom = 0;
+    while (step < pathSum) {
+        if (i < arr.length - 1 && i < arr[0].length - 1) {
+            right = arr[i][j + 1];
+            bottom = arr[i + 1][j];
+            result.push(Math.min(right, bottom))
+            if (right < bottom) {
+                j += 1;
+               
+            } else {
+                i += 1;
+            }
+            step++;
+            continue;
+        }
+        if(i == arr.length -1 && j<arr[0].length){
+            right = arr[i][j+1];
+            if(right != undefined){
+                result.push(right);
+            }
+            j += 1;
+            step++;
+        }else {
+            bottom = arr[i+1][j];
+            if(bottom != undefined){
+                result.push(bottom);
+            }
+            i +=1;
+            step++;
+        }
+       
+    }
+    result.unshift(arr[0][0]);
+    sum  = result.reduce( (a,b) => {
+        return a + b;
+    });
+    return {
+        '最短路径':result.join(' -> '),
+        '最小路径和':sum
+    };
+}
+console.log(minPathSum(
+    [
+        [1,3,1],
+        [1,5,1],
+        [4,2,1] 
+    ]
+))
