18c7f0f2c1a98ec066275aa7eca2259d8f905499
lxhyl <zhangpengfan6@gmail.com>
2020-02-06 15:50:47 +0800

    add希尔排序

---
+++ js/排序/希尔排序.js
@@ -0,0 +1,20 @@
+var arr = [3,1,2,4,5,8,6,5,3,1,4,2,5,71,12,1];
+
+function xe(e){
+  var gap =parseInt(e.length/2);
+  while(gap > 0 ){
+      for(let i = gap;i<e.length;i++){
+          var item = e[i];
+          var index = i - gap;
+          while(e[index] > item & index >= 0 ){
+              e[index + gap] = e[index];
+              index -= gap;
+          }
+          e[index + gap] = item;
+      }
+      gap = parseInt(gap/2)
+      console.log(gap)
+  }
+  return e;
+}
+console.log(xe(arr));
