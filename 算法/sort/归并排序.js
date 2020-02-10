8bd20b615c75d9db503c2bae4e9278cace98b2d7
lxhyl <zhangpengfan6@gmail.com>
2020-02-10 15:34:14 +0800

    ...

--- 算法/sort/归并排序.js
+++ 算法/sort/归并排序.js
@@ -8,27 +8,32 @@ var arr = [3,1,2,4,5,8,6,5,3,1,4,2,5,71,12,1];
         var mid = Math.floor(arr.length/2);
         var left = arr.slice(0,mid);
         var right = arr.slice(mid);
+       
         return hb(fj(left),fj(right))
     }
     //递归合并
     function hb(left,right){
+        console.log(`左:---${left}    右:---${right}`)
         var l = left && left.length,r = right && right.length;
         var i = 0,j = 0;
         var result = [];
+       
         while(i<l && j<r){
-            if(left[i] < right[j]){
+            if(left[i] < right[j])
                 result.push(left[i++])
-            }else {
+            else 
                 result.push(right[j++])
-            }
+            
         }
+        
         while(i<l){
             result.push(left[i++])
         }
         while(j<r){
             result.push(right[j++])
         }
-        return result;
+        
+         return result;
     }
 
 console.log(fj(arr));
---
+++ 算法/sort/快速排序.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+var arr = [3, 1, 2, 4, 5, 8, 6, 5, 3, 1, 4, 2, 5, 71, 12, 1];
+var i = 0, j = arr.length-1;
+console.log(ks(arr, i, j));
